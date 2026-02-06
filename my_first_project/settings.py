eaaf090ff861f47339ba75e68d0119f3a3a23da8
krishnakanthpathi <krishnakanthpathi@gmail.com>
2026-02-06 22:18:27 +0530

    add WhiteNoise middleware and configure static file storage

--- my_first_project/settings.py
+++ my_first_project/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'my_first_project.urls'
@@ -123,9 +124,13 @@ STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static')
     ]
 STATIC_ROOT=os.path.join(BASE_DIR,'assets')
-MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 #for sending email
 #SMTP configuration
--- my_first_project/urls.py
+++ my_first_project/urls.py
@@ -18,12 +18,15 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
 
     path('', include('my_app.urls')),
     path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 if settings.DEBUG:
